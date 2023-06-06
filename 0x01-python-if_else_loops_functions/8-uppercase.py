a5c4d8029bc03ca28084534ed31d8d9f72ff921e
osei <williamsabodunrin@gmail.com>
2023-06-06 13:08:42 -0700

    convert to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def uppercase(s):
-    for i in range(len(s)):
-        if ord(s[i]) >= 97 and ord(s[i]) <= 122:
-            ch = chr(ord(s[i]) - 32)
-        else:
-            ch = s[i]
-        if i == len(s) - 1:
-            print("{}".format(ch))
-        else:
-            print("{}".format(ch), end="")
+    if s == "":
+        print("")
+    else:
+        for i in range(len(s)):
+            if ord(s[i]) >= 97 and ord(s[i]) <= 122:
+                ch = chr(ord(s[i]) - 32)
+            else:
+                ch = s[i]
+            if i == len(s) - 1:
+                print("{}".format(ch))
+            else:
+                print("{}".format(ch), end="")
