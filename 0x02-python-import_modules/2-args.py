dbcf4e135f0575b7b6385bb54170fa813b602768
osei <williamsabodunrin@gmail.com>
2023-06-08 13:23:23 -0700

    add command line args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    
+    l = len(argv)
+    if l == 2:
+        print("{} argument:".format(l - 1))
+    elif l > 2:
+        print("{} arguments:".format(l - 1))
+    else:
+        print("{} arguments.".format(l - 1))
+    for i in range(len(argv[1:])):
+        print("{}: {}".format(i + 1, argv[i + 1])) 
+
