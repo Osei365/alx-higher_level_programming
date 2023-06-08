dc4292a8bd4d00049dca46a7d071e98d0f5cab6e
osei <williamsabodunrin@gmail.com>
2023-06-08 13:29:37 -0700

    command args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv)
     if length == 2:
-        print("{} argument:".format(l - 1))
+        print("{} argument:".format(lenth - 1))
     elif length > 2:
-        print("{} arguments:".format(l - 1))
+        print("{} arguments:".format(length - 1))
     else:
-        print("{} arguments.".format(l - 1))
+        print("{} arguments.".format(length - 1))
     for i in range(len(argv[1:])):
         print("{}: {}".format(i + 1, argv[i + 1]))
