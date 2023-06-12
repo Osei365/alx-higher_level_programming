9f933f76cd12d22e324c12cdf22fda757ce79165
osei <williamsabodunrin@gmail.com>
2023-06-12 03:49:14 -0700

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_ch = sentence[0]
-    if len(sentence) == 0:
-        first_ch = None
+    first_ch = None
+    if sentence:
+        first_ch = sentence[0]
     return (length, first_ch)
