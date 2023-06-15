969eb85a7b553bedbb21bd83d868d21968457047
osei <williamsabodunrin@gmail.com>
2023-06-14 19:07:40 -0700

    complex dict delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    key_list = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            key_list.append(key)
+    for i in key_list:
+        del a_dictionary[i]
+    return (a_dictionary)
