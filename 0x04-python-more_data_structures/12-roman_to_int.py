cc2687be32160233bba1da737ffe2b15ebd4dacd
osei <williamsabodunrin@gmail.com>
2023-06-14 18:36:49 -0700

    roman numerals to numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) == str and roman_string:
-        new_str = reversed(roman_string)
+        new_str = roman_string[::-1]
         rom_num = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
