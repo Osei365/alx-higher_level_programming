8544ac5511a73493422f60a257a4eec2491f488c
osei <williamsabodunrin@gmail.com>
2023-06-27 19:48:22 -0700

    coordinates square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+    """Representation of the square class."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """This initializes the square params.
+
+        Args:
+            size (int): The size of the square defined privately.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """gets the size attribute of square."""
+        return (self.__size)
+
+    @property
+    def position(self):
+        """gets the position attribute of square."""
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) == tuple and len(value) == 2:
+            if type(value[0]) == int and type(value[1]) == int:
+                pass
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        """This claculates the area of the square object.
+
+        Returns:
+            The are of the square
+        """
+        return (self.__size**2)
+
+    def my_print(self):
+        """This prints the square using hash.
+
+        Returns:
+            nothing
+        """
+        if self.size == 0:
+            print("")
+        else:
+            [print("") for i in range(self.position[1])]
+            for i in range(self.size):
+                [print(" ", end="") for n in range(self.position[0])]
+                [print("#", end="") for n in range(self.size)]
+                print("")
