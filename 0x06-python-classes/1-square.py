00f3b34ab94cef273d61c5572b5ef977f7d811dc
osei <williamsabodunrin@gmail.com>
2023-06-27 18:05:42 -0700

    Square class with private init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square class."""
+
+
+class Square:
+    """Representation of the square class."""
+
+    def __init__(self, size):
+        """This initializes the square params.
+
+        Args:
+            size (int): The size of the square defined privately.
+        """
+        self.__size = size
