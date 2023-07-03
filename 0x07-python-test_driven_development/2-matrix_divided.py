e2cc1a3d1b806cff79cb11a09a2e91a16710042e
osei <williamsabodunrin@gmail.com>
2023-07-03 06:26:58 -0700

    test driven matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,14 +5,24 @@
 def matrix_divided(matrix, div):
     """This divides a matrix.
 
+    Args:
+        matrix (list): A list of lists of ints or floats.
+        div (int/float): Divisor
+    Raises:
+        TypeError: items must be either integer or floats
+        TypeError: row of different sizes
+        TypeError: div must be integer or float
+        ZeroDivisionError: If div is 0.
+
     Returns:
        a new matrix
     """
     if (not isinstance(matrix, list) or
-                len(matrix) < 2 or
-                not all(isinstance(item, list) for item in matrix) or
-                not all(isinstance(num, (int, float)) for item in matrix for num in item)):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            len(matrix) < 2 or
+            not all(isinstance(item, list) for item in matrix) or
+            not all(isinstance(n, (int, float)) for i in matrix for n in i)):
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
     first_len = len(matrix[0])
     if (not all(len(itm_len) == first_len for itm_len in matrix[1:])):
         raise TypeError('Each row of the matrix must have the same size')
