1549b8d5e37b8b43c9167f9fe8a3f0b4acd40915
osei <williamsabodunrin@gmail.com>
2023-07-03 01:07:02 -0700

    add integer with tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/in/python3
+"""Define a function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    """This function adds two numbers.
+
+    Float arguments must be coverted to int upon addition.
+
+    Raises:
+        TypeError: if a or b are not int or floats
+    """
+
+    a_check = isinstance(a, (int, float))
+    b_check = isinstance(b, (int, float))
+    if not a_check:
+        raise TypeError('a must be an integer')
+    elif not b_check:
+        raise TypeError('b must be an integer')
+    return (int(a) + int(b))
