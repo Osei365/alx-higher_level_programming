272a4326dc8abbca1c3a47a55bbac18b309c329a
osei <williamsabodunrin@gmail.com>
2023-07-03 07:43:03 -0700

    say my name TDD

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3i
+"""Defines a function that.
+prints the firtname and lasname
+of an individual.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This prints the firstname and
+    last name of individual.
+
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name
+
+    Raises:
+        TypeError: If first and last name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("my name is {} {}".format(first_name, last_name))
