6523ff87c3a013506c643a849c7d6d9cb9bea51d
osei <williamsabodunrin@gmail.com>
2023-07-04 11:42:59 -0700

    max integer unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Represents max integerr test class."""
+
+    def test_max_integer(self):
+        """test all edge cases using max_integer function."""
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([2, 4, 6, 1, 3]), 6)
+        self.assertEqual(max_integer([34, 68, 23]), 68)
+        self.assertEqual(max_integer([9]), 9)
+        self.assertEqual(max_integer([3.4, 3.5, 3.6, 3.1]), 3.6)
