74bae81cc819836ae645c19a5a46c9f3798d353f
Osei <williamsabodunrin@gmail.com>
2023-07-05 10:27:53 +0000

    max integer TDD

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,7 +8,7 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Represents max integerr test class."""
+    """Represents max integer test class."""
 
     def test_max_integer(self):
         """test all edge cases using max_integer function."""
@@ -17,3 +17,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([34, 68, 23]), 68)
         self.assertEqual(max_integer([9]), 9)
         self.assertEqual(max_integer([3.4, 3.5, 3.6, 3.1]), 3.6)
+        self.assertEqual(max_integer([10, 9, 8, 7]), 10)
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+        self.assertEqual(max_integer([-3, 1, 3, 6, 9]), 9)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
