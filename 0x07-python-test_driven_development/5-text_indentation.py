86945745b19da9a1fbb76649622f57c9ff62f666
Osei <williamsabodunrin@gmail.com>
2023-07-08 08:10:47 +0000

    text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,11 +15,14 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError('text must be a string')
-    for i in range(len(text)):
-        if text[i] == " " and (text[i-1] in ".?:" or text[i] == '\n'):
-            continue
-        else:
-            print(text[i], end="")
-        if text[i] == '\n' or text[i] in ".?:":
-            if text[i] in ".?:":
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".:?":
+            if text[i] in ".:?":
                 print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
