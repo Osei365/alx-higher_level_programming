b389599928703ba9159119d181a7d96b171274f8
osei <williamsabodunrin@gmail.com>
2023-07-10 15:18:04 -0700

    isinstance function

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a function to check instances
+of object."""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if object is instnace else
+    returns False.
+    """
+
+    return (isinstance(obj, a_class))
