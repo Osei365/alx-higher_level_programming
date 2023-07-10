f3108292aa72101918372fea03be10f76843a3f3
osei <williamsabodunrin@gmail.com>
2023-07-10 15:43:41 -0700

    base geometry 1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Geometry."""
+
+
+class BaseGeometry:
+    """Represents the BasGeometry class."""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
