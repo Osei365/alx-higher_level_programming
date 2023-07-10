53d4a6daa1e22e534ec67323fb7abbfc93df1a41
osei <williamsabodunrin@gmail.com>
2023-07-10 15:54:33 -0700

    base geometry 2

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Geometry."""
+
+
+class BaseGeometry:
+    """Represents the BasGeometry class."""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """this validates an integer."""
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
