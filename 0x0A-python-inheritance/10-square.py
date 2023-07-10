cd86215c0d572224de8196cbda53ffbc2bf89b86
osei <williamsabodunrin@gmail.com>
2023-07-10 16:44:27 -0700

    square, rectangle, and basegeometry

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square class."""
+
+    def __init__(self, size):
+        """initiates the class quare with size.
+        instance.
+        """
+        self.__size = size
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+
+    def area(self):
+        """calculates the area of square."""
+        return (self.__size**2)
