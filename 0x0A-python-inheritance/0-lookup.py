5cd5b2590bd8ded21bdf2d338ab6a5cc8b4b5412
osei <williamsabodunrin@gmail.com>
2023-07-10 14:27:32 -0700

    print object attributes and classess

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a functioj that
+prints the attributes and methods of
+an object.
+"""
+
+
+def lookup(obj):
+    """This prints out the
+    attributes and methods of an object.
+    """
+    return (dir(obj))
