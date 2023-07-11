2e6625bb32516817399056f08c05988e47bf398e
osei <williamsabodunrin@gmail.com>
2023-07-10 17:33:43 -0700

    invert equal to and not equal to

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class My int."""
+
+
+class MyInt(int):
+    """represents the MyInt class."""
+    
+    def __eq__(self, obj):
+        """equal to method."""
+        return not self.real == obj
+
+    def __ne__(self, obj):
+        """not equal to method."""
+        return self.real == obj
