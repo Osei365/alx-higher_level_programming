6a64095b94b441dc85b4ba3d25beb752b6fd0517
osei <williamsabodunrin@gmail.com>
2023-07-11 17:51:47 -0700

    Student class, to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents the class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """initiales student class."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves dict rep of class instance."""
+        return self.__dict__
