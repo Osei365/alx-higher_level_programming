1e6b58a0a0bec793754cdbb95375093e51742ad2
osei <williamsabodunrin@gmail.com>
2023-07-11 18:11:22 -0700

    student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents the class student."""
+
+    def __init__(self, first_name, last_name, age):
+        """initiales student class."""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dict rep of class instance."""
+        if (type(attrs) == list and
+                all(isinstance(i, str) for i in attrs)):
+            return {key: self.__dict__[key] for key in attrs
+                    if key in self.__dict__.keys()}
+        return self.__dict__
