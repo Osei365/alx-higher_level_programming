6adace7a7b4733dcbaca6d304e03b78fcf87de4e
osei <williamsabodunrin@gmail.com>
2023-07-11 19:00:34 -0700

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines pascal triangle."""
+
+
+def pascal_triangle(n):
+    """returns pascal's triangle."""
+
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    if n > 2:
+        result = [[1], [1, 1]]
+        for i in range(1, n-1):
+            w = result[i]
+            new = [w[0]]
+            new.extend([w[e] + w[e+1] for e in range(i)])
+            new.append(w[-1])
+            result.append(new)
+        return result
