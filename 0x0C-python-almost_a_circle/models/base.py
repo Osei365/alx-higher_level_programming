f754ef6d2da7e741aba72b87362a161b3090bf24
Osei <williamsabodunrin@gmail.com>
2023-07-17 19:25:05 +0000

    Updating base model

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,6 @@ class Base:
 
         if (list_dictionaries is None or
                 len(list_dictionaries) == 0):
-            return '"[]"'
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
