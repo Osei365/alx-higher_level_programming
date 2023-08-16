0368ddcf7a7930853ae54a20dbb97e53be7e5348
Osei <williamsabodunrin@gmail.com>
2023-08-16 22:08:05 +0000

    count shows by genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- counts shows of genres
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_rows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_rows
 FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
