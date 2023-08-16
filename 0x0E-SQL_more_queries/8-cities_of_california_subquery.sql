c0ec03282a234aeaddbaf5824af6956ccc06c9ed
Osei <williamsabodunrin@gmail.com>
2023-08-16 19:39:50 +0000

    city of california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- select cities from states
 
 SELECT cities.id, cities.name FROM cities
-WHERE state_id = (
+WHERE states.state_id = (
 	SELECT states.id from states
 	WHERE states.name = 'California'
 )
