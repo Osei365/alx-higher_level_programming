f15c63309628522bd5f304c326d383f52cd72629
osei <williamsabodunrin@gmail.com>
2023-08-16 17:36:48 -0700

    non comedy shows

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- selects all shows without comedy genre
+
+SELECT tv_shows.title AS title FROM tv_shows
+WHERE title NOT IN
+(SELECT tv_shows.title AS title FROM tv_show_genres
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy')
+ORDER BY title;
