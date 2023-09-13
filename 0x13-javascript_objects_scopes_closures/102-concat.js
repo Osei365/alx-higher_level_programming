6d8543da5cdde342f1f99a73860f031b562c0a99
Osei <williamsabodunrin@gmail.com>
2023-09-13 02:48:59 +0000

    file read and write

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,20 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+let data = '';
+let data2 = '';
 
-const data = fs.readFileSync(process.argv[2], 'utf8');
-const data2 = fs.readFileSync(process.argv[3], 'utf8');
+try {
+  data = fs.readFileSync(process.argv[2], 'utf8');
+} catch (err) {
+  data = '';
+}
+
+try {
+  data2 = fs.readFileSync(process.argv[3], 'utf8');
+} catch (err) {
+  data = '';
+}
+
 const data3 = data + '\n' + data2 + '\n';
 fs.writeFileSync('fileC', data3);
