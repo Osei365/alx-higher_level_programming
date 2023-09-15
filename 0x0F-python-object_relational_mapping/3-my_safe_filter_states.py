664d7e2a09108f4e58241c635ff8fdb0a4a1f65d
osei <williamsabodunrin@gmail.com>
2023-09-15 07:24:27 -0700

    safe filter from user input

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+gets states from databases.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    cur = db.cursor()
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name LIKE BINARY %(name)s
+                ORDER BY id ASC
+                """, {'name': sys.argv[4]})
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
+    db.close()
