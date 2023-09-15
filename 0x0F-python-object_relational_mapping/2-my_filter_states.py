0a407fabea525ad5c643a67f4387138833d2b7d9
osei <williamsabodunrin@gmail.com>
2023-09-15 06:58:50 -0700

    filters states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cur.execute("""
                 SELECT *
                 FROM states
-                WHERE name = '{}'
+                WHERE name LIKE BINARY '{}'
                 ORDER BY id ASC
                 """.format(sys.argv[4]))
     states = cur.fetchall()
