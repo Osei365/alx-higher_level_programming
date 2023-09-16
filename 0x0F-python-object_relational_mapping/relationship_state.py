07340adc2f213aa79c37967f4ca15a886afeb164
Osei <williamsabodunrin@gmail.com>
2023-09-16 17:53:58 +0000

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,6 +6,7 @@ Satate table creation using sqlalchemy
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
