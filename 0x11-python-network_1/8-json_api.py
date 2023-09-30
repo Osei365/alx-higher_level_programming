914a6a81966d764de21a89f0eb8ab81422f2d2a9
Osei <williamsabodunrin@gmail.com>
2023-09-30 00:06:36 +0000

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(ret.get('id'), ret.get('name')))
-    except requests.exceptions.JSONDecodeError:
+    except ValueError:
         print('Not a valid JSON')
