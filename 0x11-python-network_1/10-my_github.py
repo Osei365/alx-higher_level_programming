8e9e19260ec1a10278b215995e70cf37ad5f6882
Osei <williamsabodunrin@gmail.com>
2023-09-30 00:57:05 +0000

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,18 +2,12 @@
 """github api"""
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    headers = {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': 'Bearer {}'.format(password),
-            'X-GitHub-Api-Version': '2022-11-28'
-            }
-    r = requests.post('https://api.github.com/user', headers=headers)
-    if 'id' in r.json():
-        print(r.json()['id'])
-    else:
-        print(None)
+    auth = HTTPBasicAuth(username, password)
+    r = requests.post('https://api.github.com/user', auth=auth)
+    print(r.json()['id'])
