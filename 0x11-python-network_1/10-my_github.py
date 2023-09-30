ff23db054e8052350fc5b1db4f000ad6aec7add2
Osei <williamsabodunrin@gmail.com>
2023-09-30 00:59:20 +0000

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     auth = HTTPBasicAuth(username, password)
-    r = requests.post('https://api.github.com/user', auth=auth)
+    r = requests.get('https://api.github.com/user', auth=auth)
     print(r.json()['id'])
