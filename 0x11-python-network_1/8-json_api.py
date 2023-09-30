87919a0000047ef0bc578d9d4f0a2bdc85c4d3e1
Osei <williamsabodunrin@gmail.com>
2023-09-29 17:02:55 -0700

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""json response content"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        arg = ""
+    else:
+        arg = sys.argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': arg})
+    try:
+        ret = r.json()
+        if len(ret) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(ret['id'], ret['name']))
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
