47e8ca20ec56b24f89aba1659ef21828f6f68fdf
Osei <williamsabodunrin@gmail.com>
2023-10-24 16:04:08 -0700

    starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,9 +4,11 @@ const request = require('request');
 const ch = process.argv[2];
 request.get(ch, function (err, response, body) {
   if (!err) {
-    const results = console.log(JSON.parse(body).results);
+    const results = JSON.parse(body).resultsi;
     console.log(results.reduce((count, content) => {
       return content.characters.find((character) => character.endsWith('/18/'))
-      ? count + 1 : count}, 0));
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
