b327eaee67ec1a75811a838024ca4cf781d931f8
Osei <williamsabodunrin@gmail.com>
2023-10-24 16:47:54 -0700

    get completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (err, response, body) {
+  if (!err) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos[i];
+      if (todo.completed === true) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  }
+});
